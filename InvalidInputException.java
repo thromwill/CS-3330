2a39fa3d5ea7a165d7a882ec64e06810c32f9d9b
Will Throm <94716393+thromwill@users.noreply.github.com>
2022-05-11 08:21:45 -0500

    Add files via upload

---
+++ InvalidInputException.java
@@ -0,0 +1,12 @@
+//----------------------------------------------------------------------------------------Class InvalidInputException
+
+public class InvalidInputException extends RuntimeException {
+    
+    public InvalidInputException(String errorMessage) {
+        super(errorMessage);
+    }
+    
+    
+}
+
+//----------------------------------------------------------------------------------------Class InvalidInputException
